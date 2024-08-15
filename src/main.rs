08d3b927b65f40a2ec94cf9e998f1657746efa9c
Dharmik Kaku <126943942+dharmik2319@users.noreply.github.com>
2024-08-16 04:19:15 +0530

    Added support for scripts; added scripts.ron (#25)

--- src/main.rs
+++ src/main.rs
@@ -52,16 +52,32 @@ struct Services {
     list: Vec<Service>,
 }
 
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Script {
+    name: String,
+    description: String,
+    repo: String,
+    image: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Scripts {
+    list: Vec<Script>,
+}
+
 fn main() -> anyhow::Result<()> {
     let applications_data = include_str!("../applications.ron");
     let applets_data = include_str!("../applets.ron");
     let themes_data = include_str!("../themes.ron");
     let services_data = include_str!("../services.ron");
+    let scripts_data = include_str!("../scripts.ron");
     let mut readme = String::new();
     let applications: Applications = ron::from_str(applications_data).unwrap();
     let applets: Applets = ron::from_str(applets_data).unwrap();
     let themes: Themes = ron::from_str(themes_data).unwrap();
     let services: Services = ron::from_str(services_data).unwrap();
+    let scripts: Scripts = ron::from_str(scripts_data).unwrap();
 
     readme.push_str("# COSMIC Project Collection\n");
     readme.push_str("A collection of COSMIC projects developed by the community.\n\n");
@@ -71,6 +87,7 @@ fn main() -> anyhow::Result<()> {
 
     write_services(&mut readme, services);
     write_themes(&mut readme, themes);
+    write_scripts(&mut readme, scripts);
 
     readme.push_str("\n## How to add your project?\n");
     readme.push_str("To add your project to this list, please open a pull request with your project added to the `applications.ron` or `applets.ron` file.\n");
@@ -183,6 +200,34 @@ fn write_services(readme: &mut String, services: Services) {
                 row.push_str(&format!(
                     " <img src=\"{}\" alt=\"{}\" width=\"200\"/> |\n",
                     image, service.name
+                ));
+            }
+            None => row.push_str(" |\n"),
+        }
+
+        readme.push_str(&row);
+    }
+}
+
+fn write_scripts(readme: &mut String, scripts: Scripts) {
+    readme.push_str("\n## Scripts\n");
+
+    readme.push_str("| Name | Description | Image |\n");
+    readme.push_str("|---|---|---|\n");
+
+    for script in scripts.list {
+        let mut row = String::new();
+
+        row.push_str(&format!(
+            "| [{}]({}) | {} |",
+            script.name, script.repo, script.description
+        ));
+
+        match script.image {
+            Some(image) => {
+                row.push_str(&format!(
+                    " <img src=\"{}\" alt=\"{}\" width=\"200\"/> |\n",
+                    image, script.name
                 ));
             }
             None => row.push_str(" |\n"),
